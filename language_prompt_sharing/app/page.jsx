b3b2ae68af738c9add321c20d269d20ee6deaded
smannasa98 <seanmannas02@gmail.com>
2023-10-22 19:16:26 -0400

    most of the UI structure

---
+++ language_prompt_sharing/app/page.jsx
@@ -0,0 +1,20 @@
+import Feed from '@components/Feed';
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        This is a prompt sharing app testing
+        the capabilities of new language models.
+      </p>
+      <Feed />
+    </section>
+  )
+}
+
+export default Home
